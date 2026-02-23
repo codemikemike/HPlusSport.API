0de422c3c0d784935b3c04215852a93ec0d227b7
codemikemike <mikr71394@edu.ucl.dk>
2026-02-23 04:36:08 +0100

    add minimal API code

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -15,21 +15,17 @@ namespace HPlusSport.API.Controllers
         {
             _context = context;
 
-            // Sikrer at In-Memory databasen er oprettet og Seed-data er indlæst
             _context.Database.EnsureCreated();
         }
 
-        // Henter alle produkter: GET api/products
         [HttpGet]
         public async Task<ActionResult> GetAllProducts()
         {
             return Ok(await _context.Products.ToArrayAsync());
         }
 
-        // Henter et specifikt produkt: GET api/products/1
-        // Bemærk {id} med krølleparenteser - det gør det til en variabel
         [HttpGet("{id}")]
-        public async Task<ActionResult> GetProductById(int id)
+        public async Task<ActionResult> GetProduct(int id)
         {
             var product = await _context.Products.FindAsync(id);
             if (product == null)
@@ -39,39 +35,37 @@ namespace HPlusSport.API.Controllers
             return Ok(product);
         }
 
-        // Henter tilgængelige produkter: GET api/products/available
         [HttpGet("available")]
-        public async Task<ActionResult> GetAvailableProducts()
+        public async Task<ActionResult<IEnumerable<Product>>> GetAvailableProducts()
         {
-            var products = await _context.Products.Where(p => p.IsAvailable).ToArrayAsync();
-            return Ok(products);
+            return await _context.Products.Where(p => p.IsAvailable).ToArrayAsync();
         }
 
-        // Opretter et nyt produkt: POST api/products
         [HttpPost]
         public async Task<ActionResult> PostProduct(Product product)
         {
-
+            /*
             if (!ModelState.IsValid)
             {
-                return BadRequest(ModelState);
+                return BadRequest();
             }
+            */
 
             _context.Products.Add(product);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction(
-                nameof(GetProductById), // Navnet på metoden ovenfor
-                new { id = product.Id }, // ID'et på det nye produkt
+                nameof(GetProduct),
+                new { id = product.Id },
                 product);
         }
 
-        [HttpPut("{id}")] // Rettet: Ruten skal stå inde i parentesen
+        [HttpPut("{id}")]
         public async Task<ActionResult> PutProduct(int id, Product product)
         {
             if (id != product.Id)
             {
-                return BadRequest("ID i URL og body skal være ens.");
+                return BadRequest();
             }
 
             _context.Entry(product).State = EntityState.Modified;
@@ -91,21 +85,45 @@ namespace HPlusSport.API.Controllers
                     throw;
                 }
             }
+
             return NoContent();
         }
 
-            [HttpDelete("{id}")] // Rettet: Ruten skal stå inde i parentesen
-            public async Task<ActionResult> DeleteProduct(int id)
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        [HttpPost("Delete")]
+        public async Task<ActionResult> DeleteMultiple([FromQuery] int[] ids)
+        {
+            var products = new List<Product>();
+            foreach (var id in ids)
             {
                 var product = await _context.Products.FindAsync(id);
+
                 if (product == null)
                 {
                     return NotFound();
                 }
-                _context.Products.Remove(product);
-                await _context.SaveChangesAsync();
-                return Ok(product);
-        }
 
-    } // Closes the Class
-} // Closes the Namespace
+                products.Add(product);
+            }
+
+            _context.Products.RemoveRange(products);
+            await _context.SaveChangesAsync();
+
+            return Ok(products);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,24 @@
 using HPlusSport.API.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers()
+
+builder.Services.AddControllers()/*
     .ConfigureApiBehaviorOptions(options =>
     {
-        // Deaktiverer automatisk 400-fejl fra [ApiController]. 
-        // Gør det muligt selv at styre valideringslogikken i controlleren.
         options.SuppressModelStateInvalidFilter = true;
-    });
-
+    })*/;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ShopContext>(options =>
-    options.UseInMemoryDatabase("ShopDb"));
+{
+    options.UseInMemoryDatabase("Shop");
+});
 
 var app = builder.Build();
 
@@ -37,15 +38,14 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ShopContext>();
-        await db.Database.EnsureCreatedAsync();
+    await db.Database.EnsureCreatedAsync();
 }
 
-    app.MapGet("/Products", async (ShopContext _context) =>
-    {
-        return await _context.Products.ToArrayAsync();
-    });
+app.MapGet("/products", async (ShopContext _context) =>
+{
+    return await _context.Products.ToArrayAsync();
+});
 
-// Det første endpoint er faktisk korrekt, hvis _context er stavet rigtigt
 app.MapGet("/products/{id}", async (int id, ShopContext _context) =>
 {
     var product = await _context.Products.FindAsync(id);
@@ -54,12 +54,83 @@ app.MapGet("/products/{id}", async (int id, ShopContext _context) =>
         return Results.NotFound();
     }
     return Results.Ok(product);
+}).WithName("GetProduct");
+
+app.MapGet("/products/available", async (ShopContext _context) =>
+    Results.Ok(await _context.Products.Where(p => p.IsAvailable).ToArrayAsync())
+);
+
+app.MapPost("/products", async (ShopContext _context, Product product) =>
+{
+    _context.Products.Add(product);
+    await _context.SaveChangesAsync();
+
+    return Results.CreatedAtRoute(
+        "GetProduct",
+        new { id = product.Id },
+        product);
 });
 
-// Det andet endpoint rettet for stavefejl og syntaks
-app.MapGet("/products/available", async (ShopContext _context) =>
+app.MapPut("/products/{id}", async (ShopContext _context, int id, Product product) =>
 {
-    var products = await _context.Products.Where(p => p.IsAvailable).ToArrayAsync();
+    if (id != product.Id)
+    {
+        return Results.BadRequest();
+    }
+
+    _context.Entry(product).State = EntityState.Modified;
+
+    try
+    {
+        await _context.SaveChangesAsync();
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+        if (!_context.Products.Any(p => p.Id == id))
+        {
+            return Results.NotFound();
+        }
+        else
+        {
+            throw;
+        }
+    }
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/products/{id}", async (ShopContext _context, int id) =>
+{
+    var product = await _context.Products.FindAsync(id);
+    if (product == null)
+    {
+        return Results.NotFound();
+    }
+
+    _context.Products.Remove(product);
+    await _context.SaveChangesAsync();
+
+    return Results.Ok(product);
+});
+
+app.MapPost("/products/Delete", async (ShopContext _context, [FromQuery] int[] ids) =>
+{
+    var products = new List<Product>();
+    foreach (var id in ids)
+    {
+        var product = await _context.Products.FindAsync(id);
+
+        if (product == null)
+        {
+            return Results.NotFound();
+        }
+
+        products.Add(product);
+    }
+
+    _context.Products.RemoveRange(products);
+    await _context.SaveChangesAsync();
+
     return Results.Ok(products);
 });
 
